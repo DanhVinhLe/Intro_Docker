9d58dd2f8ca4955857bd0f45fe7a71b33bd20f39
danhvinhle-zenai <danhvinhle.zenai@gmail.com>
2025-07-06 21:45:49 +0700

    Add class request

--- app.py
+++ app.py
@@ -17,16 +17,19 @@ pipe = StableDiffusionPipeline.from_single_file(
 )
 pipe.to("cuda")
 
+class SD_Request(BaseModel):
+    prompt: str
+    width: int = 512
+    height: int = 512
+
 @app.post("/generate")
-def generate_image(prompt: str = Form(...),
-                   width: int = Form(512),
-                   height: int = Form(512)):
+def generate_image(request: SD_Request):
     try:
-        if width < 128 or width > 1024 or height < 128 or height > 1024:
+        if request.width < 128 or request.width > 1024 or request.height < 128 or request.height > 1024:
             raise HTTPException(status_code=400, detail="Width and height must be between 128 and 1024")
-        width = (width // 8) * 8
-        height = (height // 8) * 8
-        image = pipe(prompt, width = width, height = height).images[0]
+        width = (request.width // 8) * 8
+        height = (request.height // 8) * 8
+        image = pipe(prompt=request.prompt, width=width, height=height).images[0]
         buffered = io.BytesIO()
         image.save(buffered, format="PNG")
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
